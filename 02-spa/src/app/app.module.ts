b3f0994d1a2a7e850e03bca7213cbcb1a26c9b88
Victor Rigoberto  Jose Santiago <jsvicctor@gmail.com>
2020-04-15 14:16:46 -0500

    Avances

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { APP_ROUTING } from './app.routes';
+import { HeroesService } from './services/heroes.service';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
+
 
 
 
@@ -16,13 +20,16 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ 02-spa/src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../services/heroes.service';
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styles: []
+})
+export class HeroeComponent implements OnInit {
+  heroe: any = {};
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private heroesService: HeroesService
+    ) {
+    this.activatedRoute.params.subscribe( params => {
+      this.heroe = this.heroesService.getHeroe(params.id);
+      console.log(this.heroe);
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
