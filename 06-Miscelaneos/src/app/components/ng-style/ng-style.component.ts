33230f6c1c31b3a6e36e819cf64fb5f6cd5a05c5
Victor Rigoberto  Jose Santiago <jsvicctor@gmail.com>
2020-05-19 13:49:55 -0500

    Avances
    Se instala JQuery, Tether, y FontAwesome. Se avanza hasta Directivas Personalizadas

---
+++ 06-Miscelaneos/src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+  <p [style.fontSize.px]="tamano">
+    Hola Mundo... Esta es una etiqueta HTML
+  </p>
+
+  <button class="btn btn-primary" (click)="tamano = tamano + 5">
+   <i class="fa fa-plus"></i>
+  </button>
+
+  <button class="btn btn-primary ml-1" (click)="tamano = tamano - 5">
+   <i class="fa fa-minus"></i>
+  </button>
+
+  `,
+  styles: []
+})
+export class NgStyleComponent implements OnInit {
+
+  tamano = 30;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
