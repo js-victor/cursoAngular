59543e93ba6d7b6f4cf8988d8f0f4e109e734c1d
Victor Rigoberto  Jose Santiago <jsvicctor@gmail.com>
2020-05-19 18:25:53 -0500

    Avances
    Avances hasta ngSwitch

--- 06-Miscelaneos/src/app/directives/resaltado.directive.ts
+++ 06-Miscelaneos/src/app/directives/resaltado.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appResaltado]'
 })
 export class ResaltadoDirective {
+  @Input('appResaltado') nuevoColor: string;
 
-  constructor() { }
+  @HostListener('mouseenter') mouseEntro() {
+    this.resaltar(this.nuevoColor || 'yellow');
+    // this.elementRef.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseleave') mouseSalio() {
+    this.resaltar(null);
+    // this.elementRef.nativeElement.style.backgroundColor = null;
+    // console.log(this.nuevoColor);
+  }
+
+  constructor(private elementRef: ElementRef) {
+    // console.log('Directiva llamada');
+    // elementRef.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  private resaltar(color: string) {
+    this.elementRef.nativeElement.style.backgroundColor = color;
+
+  }
+
+
 
 }
